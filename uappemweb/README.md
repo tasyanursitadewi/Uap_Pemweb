bbd9340f1ed75abc0e9cad8b96418b36978be7d6
tasyanursitadewi <62372670+tasyanursitadewi@users.noreply.github.com>
2021-06-23 11:00:24 +0700

    Add files via upload

---
+++ uappemweb/README.md
@@ -0,0 +1,1 @@
+# Uap_Pemweb
---
+++ uappemweb/delete.php
@@ -0,0 +1,25 @@
+<?php
+
+include('config.php');
+
+
+if(isset($_GET['Nim'])){
+	$Nim = $_GET['Nim'];
+	$cek = mysqli_query($koneksi, "SELECT * FROM mahasiswa WHERE Nim='$Nim'") or die(mysqli_error($koneksi));
+
+
+	if(mysqli_num_rows($cek) > 0){
+		$del = mysqli_query($koneksi, "DELETE FROM mahasiswa WHERE Nim='$Nim'") or die(mysqli_error($koneksi));
+		if($del){
+			echo '<script>alert("Berhasil menghapus data."); document.location="index.php?page=tampil_mhs";</script>';
+		}else{
+			echo '<script>alert("Gagal menghapus data."); document.location="index.php?page=tampil_mhs";</script>';
+		}
+	}else{
+		echo '<script>alert("ID tidak ditemukan di database."); document.location="index.php?page=tampil_mhs";</script>';
+	}
+}else{
+	echo '<script>alert("ID tidak ditemukan di database."); document.location="index.php?page=tampil_mhs";</script>';
+}
+
+?>
